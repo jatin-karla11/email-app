ab853011d7d2f8e0ebcb3397b7c51b527b4b0a7c
Jatin Karla <jatinkarla8@gmail.com>
2021-01-08 20:57:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -68,19 +68,7 @@ function App() {
 
 <nav className="navbar">
   <ul>
-      <button>🞬</button>
-    <li> 
       
-      <a href="#aboutMe"> About Me </a>
-    </li>
-    <li>
-      
-       <a href="#projects"> Projects </a>
-    </li>
-    <li>
-      
-      <a href="#cv"> CV </a>
-    </li>
     <li>
       
       <a href="#contacts"> Contacts </a>
